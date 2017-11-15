f0074fd0d3444701ded08adf63567512c704f39c
Gordon Schulz <gordon.schulz@gmail.com>
2017-11-15 15:23:02 +0100

    Streamline testing

--- molecule/default/tests/test_vscode.py
+++ molecule/default/tests/test_vscode.py
@@ -1,34 +1,34 @@
 import os
 import testinfra.utils.ansible_runner
 
-
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_vscode_package(host):
+def test_vscode_package_isinstalled(host):
+    """ Test whether the code package is installed. """
     assert host.package("code").is_installed
 
 
-def test_vscode_isinstalled(host):
-    assert host.file("/usr/bin/code").exists and \
-        host.file("/usr/bin/code").is_file
+def test_vscode_isinpath(host):
+    """ Test whether the code command is in PATH. """
+    assert host.exists("code")
 
 
 def test_vscode_mayrun(host):
-    command = "/usr/bin/code --user-data-dir=/tmp --help"
+    """ Test general extension of the code command. """
+    command = "code --user-data-dir=/tmp --help"
     assert "Visual Studio Code" in host.command(command).stdout.split("\n")[0]
 
 
 def test_extension_isinstalled(host):
-    molecule_testuser = os.environ['molecule_testuser']
-    molecule_testextension = os.environ['molecule_extension']
-    assert molecule_testuser is not None
-    assert molecule_testextension is not None
+    """ Test whether requested extensions installed correctly """
+    users = host.ansible.get_variables()['vscode_extensions']
+    for user in users:
+        home = host.user(user['user']).home
+        extensions_directory = "{0}/.vscode/extensions".format(home)
+        assert host.file(extensions_directory).is_directory
 
-    extensions_directory = \
-        "/home/{0}/.vscode/extensions".format(molecule_testuser)
-    assert host.file(extensions_directory).is_directory
-
-    entries = host.check_output("ls -1 {0}".format(extensions_directory))
-    assert molecule_testextension in entries
+        entries = host.check_output("ls -1 {0}".format(extensions_directory))
+        for extension in user['extensions']:
+            assert extension in entries
